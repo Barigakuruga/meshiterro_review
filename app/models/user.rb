69f44d65dedbf74320133b3ebdf3e3b76cd165b1
EC2 Default User <ec2-user@ip-172-31-31-120.ap-northeast-1.compute.internal>
2023-03-15 01:55:08 +0000

    [save]ユーザー画像の設定まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :post_images, dependent: :destroy
+  has_one_attached :profile_image
+  
+  def get_profile_image(w,h)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    else
+      profile_image.variant(resize_to_limit: [w,h]).processed
+    end
+  end
+  
 end
